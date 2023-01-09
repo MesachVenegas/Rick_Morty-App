799a84166e5dfe27ca6c709be4d655783692e1bc
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-01-08 20:10:58 -0800

    changes: info render error

--- src/assets/Components/BoxContent/BoxContent.jsx
+++ src/assets/Components/BoxContent/BoxContent.jsx
@@ -7,31 +7,38 @@ import './boxContenten.css'
 
 const BoxContent = () => {
     const [location, setLocation] = useState([]);
-    const [randomLocation, setRandomLocation] = useState({});
+    const [randomLocation, setRandomLocation] = useState(1);
 
     const getLocation = async () => {
         await rickApi.get('/location')
             .then(res => setLocation(res.data.results))
-        }
+            .then(
+                )
+    }
 
+    const genLocation = async () =>{
+        const locationRandom =  Math.floor(Math.random() * location.length)
+        setRandomLocation(locationRandom)
+    }
 
     useEffect( () =>{
         getLocation()
-        setRandomLocation( location?.[ Math.floor( Math.random() * location.length ) ])
+        genLocation()
     },[])
 
+    console.log(location[randomLocation]);
 
-    // console.log(location);
-    console.log(randomLocation);
 
     return (
         <div className='flex__container content'>
-            <LocationInfo key={randomLocation?.id} name={randomLocation?.name} type={randomLocation?.type} residents={randomLocation?.residents}/>
-            <hr />
-            <h1>Caja de Contenido</h1>
-            <hr/>
-            <div className="card__item">
-                <h3 className="name">Nombre: </h3>
+            <div className="locationInfo">
+                <LocationInfo loc={location[randomLocation]}/>
+            </div>
+            <div className="dataContaint">
+                <h1>Caja de Contenido</h1>
+                <div className="card__item">
+                    <h3 className="name">Nombre: </h3>
+                </div>
             </div>
         </div>
     );
--- src/assets/Components/LocationInfo/Locationinfo.jsx
+++ src/assets/Components/LocationInfo/Locationinfo.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import React from 'react';
 
-const Locationinfo = ({name, type, dimension, residents}) => {
-    return (
-        <div className='flex__container info__location'>
-            <h3>{name}</h3>
-            <ul className="content__location">
-                <li>Type: {type ? type : 'Unknown'}</li>
-                <li>Dimension: {dimension ? dimension : 'Unknown'}</li>
-                <li>Population: {residents.length}</li>
-            </ul>
-            <hr />
-        </div>
-    );
-};
+const Locationinfo = ({randomLocation}) => {
+    const [loc, setLoc] = useState('')
+    setLoc(randomLocation)
+    console.log(loc);
+
+    if(!loc){
+            return null;
+        }
+        else{
+            return(
+                <div className='flex__container info__location'>
+                    <h3>{loc.name}</h3>
+                    <ul className="content__location">
+                        <li>Type: {loc.type}</li>
+                        <li>Dimension: {loc.dimension} </li>
+                        <li>Population: {loc.residents}</li>
+                    </ul>
+                    <hr />
+                </div>
+            )
+        }
+}
 
 export default Locationinfo;
